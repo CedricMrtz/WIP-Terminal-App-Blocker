a7d15f8f4ad5ae66fdac2ab2c13f8117e9feed94
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2025-12-14 09:39:56 -0600

    refactor: change files distribution

--- terminal-app-blocker/target/debug/deps/terminal_app_blocker-69c9fa87e426b048.d
+++
@@ -1,8 +0,0 @@
-/Users/cedricmartinez/Downloads/Terminal-App-Blocker/terminal-app-blocker/target/debug/deps/terminal_app_blocker-69c9fa87e426b048.d: src/main.rs src/engine.rs src/rule.rs src/target.rs
-
-/Users/cedricmartinez/Downloads/Terminal-App-Blocker/terminal-app-blocker/target/debug/deps/terminal_app_blocker-69c9fa87e426b048: src/main.rs src/engine.rs src/rule.rs src/target.rs
-
-src/main.rs:
-src/engine.rs:
-src/rule.rs:
-src/target.rs:
