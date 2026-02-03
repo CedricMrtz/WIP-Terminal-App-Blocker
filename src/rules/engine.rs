6ffc7e0ee46e6faf5c34d990f39ef18bccd5d585
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2026-02-02 17:23:03 -0800

    feat: read /etc/hosts in engine

--- src/rules/engine.rs
+++ src/rules/engine.rs
@@ -1,29 +1,36 @@
-// use crate::rules::target::Target;
-// use crate::rules::rule::{Rule, Action};
+use crate::rules::target::Target;
+use crate::rules::rule::{Rule, Action};
+use std::io::Error;
+use std::fs;
 
-// pub struct Engine{
-//     rules: Vec<Rule>,
-// }
+pub struct Engine{
+    rules: Vec<Rule>,
+    fileinfo: String,
+}
 
-// impl Engine {
-//     pub fn new(rules: Vec<Rule>) -> Self{
-//         Self { rules }
-//     }
+impl Engine {
+    pub fn new(rules: Vec<Rule>) -> Result<Self, Error>{
+        let fileinfo = fs::read_to_string("/etc/hosts")?;
+        Ok(Self{
+            rules,
+            fileinfo,
+        })
+    }
 
-//     pub fn check(&self, target: &Target) -> Action{
-//         for rule in &self.rules{
-//             if Engine::matches(&rule.target, target){
-//                 return rule.action.clone();
-//             }
-//         }
-//         Action::Allow
-//     }
+    // pub fn check(&self, target: &Target) -> Action{
+    //     for rule in &self.rules{
+    //         if Engine::matches(&rule.target, target){
+    //             return rule.action.clone();
+    //         }
+    //     }
+    //     Action::Allow
+    // }
 
-//     fn matches(rule_target: &Target, input: &Target) -> bool{
-//         match(rule_target, input){
-//             (Target::Website(a), Target::Website(b)) => a == b,
-//             (Target::Application(a), Target::Application(b)) => a == b,
-//             _ => false,
-//         }
-//     }
-// }
+    fn matches(rule_target: &Target, input: &Target) -> bool{
+        match(rule_target, input){
+            (Target::Website(a), Target::Website(b)) => a == b,
+            (Target::Application(a), Target::Application(b)) => a == b,
+            _ => false,
+        }
+    }
+}
